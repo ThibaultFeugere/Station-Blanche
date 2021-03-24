5ee2f8774ec4f550e0dafb777fe9ce1f31fec027
Thibault FEUGERE <thibaultfeugere@gmail.com>
2021-03-24 09:57:24 +0000

    Update installs/install-virus-total-gui.sh

--- installs/install-virus-total-gui.sh
+++ installs/install-virus-total-gui.sh
@@ -2,7 +2,7 @@
 sudo apt-get install git build-essential qtchooser qt5-default libjansson-dev libcurl4-openssl-dev git zlib1g-dev
 
 # clone the c-vtapi library
-git clone https://github.com/VirusTotal/c-vtapi.git /home/stationblanche/Bureau
+git clone https://github.com/VirusTotal/c-vtapi.git /home/stationblanche/Bureau/c-vtapi
 
 #change to c-vtapi directory
 cd /home/stationblanche/Bureau/c-vtapi
@@ -24,7 +24,7 @@ sudo ldconfig
 cd ..
 
 #clone QT VirusTotal Uplaoder
-git clone https://github.com/VirusTotal/qt-virustotal-uploader.git /home/stationblanche/Bureau
+git clone https://github.com/VirusTotal/qt-virustotal-uploader.git /home/stationblanche/Bureau/qt-virustotal-uploader
 cd /home/stationblanche/Bureau/qt-virustotal-uploader
 
 # run qmake, specifing qt5 
