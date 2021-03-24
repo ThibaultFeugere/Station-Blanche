3a54aab40fdc09e4b864ab937e254044c4ae4b45
Thibault FEUGERE <thibaultfeugere@gmail.com>
2021-03-24 10:27:28 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,5 +3,7 @@
 echo "Mise à jour du système en cours"
 sudo apt update && sudo apt-upgrade -y
 
+echo "Arrêt du service ClamAV"
+sudo systemctl stop clamav-freshclam.service
 echo "Mise à jour de la base de données de signatures ClamAV"
 fresclam
