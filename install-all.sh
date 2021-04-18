40c77fe687dcb4748b97241571b5bbc3d5182d3e
Thibault FEUGERE <thibaultfeugere@gmail.com>
2021-04-18 14:08:24 +0000

    Update install-all.sh

--- install-all.sh
+++ install-all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Installation des dépendances"
+echo "Installation des dépendances nécessaires"
 sudo apt-get install -y git apt-transport-https build-essential qtchooser qt5-default libjansson-dev libcurl4-openssl-dev git zlib1g-dev
 
 echo "Installation des outils nécessaires à la station blanche"
